88167290d3ee133c067efd2c5b9921419dc171d7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-06 12:14:59 -0500

    remove unnecessary code and add test

--- src/render/render-footer.js
+++ src/render/render-footer.js
@@ -1,67 +1,91 @@
 const h = require('virtual-dom/h')
 
-function hashFragment () {
-  return typeof window !== 'undefined' && window.location.hash.split('/')[1] || ''
+function hashFragment() {
+  return (
+    (typeof window !== 'undefined' && window.location.hash.split('/')[1]) || ''
+  )
 }
 
-function countRemaining (todos) {
-  return todos.length - todos.reduce(function (count, todo) {
-    return count + Number(todo.done)
-  }, 0)
+function countRemaining(todos) {
+  return (
+    todos.length -
+    todos.reduce(function (count, todo) {
+      return count + Number(todo.done)
+    }, 0)
+  )
 }
 
-function hasCompleted (todos) {
-  return todos && todos.some(function (todo) {
-    return todo.done
-  })
+function hasCompleted(todos) {
+  return (
+    todos &&
+    todos.some(function (todo) {
+      return todo.done
+    })
+  )
 }
 
 // url could be nothing or '/active', '/completed'
-function render (Todos, url) {
+function render(Todos, url) {
   const remaining = countRemaining(Todos.items)
   const route = hashFragment() || url
 
-  return h('footer', {className: 'footer'}, [
-    h('span', {className: 'todo-count'}, [
+  return h('footer', { className: 'footer' }, [
+    h('span', { className: 'todo-count' }, [
       h('strong', {}, String(remaining)),
-      ' items left'
+      ' items left',
     ]),
-    h('ul', {className: 'filters'}, [
+    h('ul', { className: 'filters' }, [
       h('li', [
-        h('a', {
-          className: !route ? 'selected' : '',
-          href: '/'
-        }, 'All')
+        h(
+          'a',
+          {
+            className: !route ? 'selected' : '',
+            href: '/',
+          },
+          'All',
+        ),
       ]),
       h('li', [
-        h('a', {
-          className: route === '/active' ? 'selected' : '',
-          href: '/active'
-        }, 'Active')
+        h(
+          'a',
+          {
+            className: route === '/active' ? 'selected' : '',
+            href: '/active',
+          },
+          'Active',
+        ),
       ]),
       h('li', [
-        h('a', {
-          className: route === '/completed' ? 'selected' : '',
-          href: '/completed'
-        }, 'Completed')
-      ])
+        h(
+          'a',
+          {
+            className: route === '/completed' ? 'selected' : '',
+            href: '/completed',
+          },
+          'Completed',
+        ),
+      ]),
     ]),
-    h('form', {
-      method: 'POST',
-      action: '/clear-completed',
-      enctype: 'application/x-www-form-urlencoded'
-    }, [
-      h('button', {
-        className: 'clear-completed',
-        style: {
-          display: hasCompleted(Todos.items) ? 'block' : 'none'
-        },
-        onclick: function () {
-          // todos && todos.clearCompleted();
-          // renderApp();
-        }
-      }, 'Clear completed')
-    ])
+    h(
+      'form',
+      {
+        method: 'POST',
+        action: '/clear-completed',
+        enctype: 'application/x-www-form-urlencoded',
+      },
+      [
+        h(
+          'button',
+          {
+            className: 'clear-completed',
+            style: {
+              display: hasCompleted(Todos.items) ? 'block' : 'none',
+            },
+          },
+          'Clear completed',
+        ),
+      ],
+    ),
   ])
 }
 
