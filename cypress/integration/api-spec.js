8208ff39f4c00be4832bf5b8a723a5cd86e84c76
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-06 11:42:03 -0500

    test add todo without what property

--- cypress/integration/api-spec.js
+++ cypress/integration/api-spec.js
@@ -14,36 +14,51 @@ describe('TodoMVC API', () => {
     cy.request('POST', '/reset')
   })
 
-  it('adds a todo', () => {
-    cy.request('/todos').its('body').should('have.length', 0)
-    cy.request('POST', '/', {
-      what: 'new todo',
-    })
-    cy.request('/todos')
-      .its('body')
-      .should('have.length', 1)
-      .its('0')
-      .should('include', {
+  context('add', () => {
+    it('adds a todo', () => {
+      cy.request('/todos').its('body').should('have.length', 0)
+      cy.request('POST', '/', {
         what: 'new todo',
+      })
+      cy.request('/todos')
+        .its('body')
+        .should('have.length', 1)
+        .its('0')
+        .should('include', {
+          what: 'new todo',
+          done: false,
+        })
+        .and('have.property', 'id')
+        // our uuid is lowercase
+        .should(
+          'match',
+          /^[0-9a-f]{8}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{12}$/,
+        )
+
+      cy.log('**render HTML**')
+      cy.request('/').its('body').then(writeHtml)
+
+      // now that the server response is in the test runner
+      // let's query it like a normal site
+      cy.get('.todo-list li')
+        .should('have.length', 1)
+        .first()
+        .find('label')
+        .should('have.text', 'new todo')
+    })
+
+    it('does not add a todo without "what" property', () => {
+      cy.request('/todos').its('body').should('have.length', 0)
+      cy.request('POST', '/', {
         done: false,
       })
-      .and('have.property', 'id')
-      // our uuid is lowercase
-      .should(
-        'match',
-        /^[0-9a-f]{8}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{12}$/,
-      )
-
-    cy.log('**render HTML**')
-    cy.request('/').its('body').then(writeHtml)
-
-    // now that the server response is in the test runner
-    // let's query it like a normal site
-    cy.get('.todo-list li')
-      .should('have.length', 1)
-      .first()
-      .find('label')
-      .should('have.text', 'new todo')
+      cy.request('/todos').its('body').should('have.length', 0)
+      cy.request('POST', '/', {
+        what: 'has text',
+        done: false,
+      })
+      cy.request('/todos').its('body').should('have.length', 1)
+    })
   })
 
   context('delete', () => {
