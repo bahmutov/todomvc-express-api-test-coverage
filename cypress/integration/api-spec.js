087205bc35e89038d17e750552512bb138db2128
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-06 10:39:49 -0500

    add special pages tests (#4)

--- cypress/integration/api-spec.js
+++ cypress/integration/api-spec.js
@@ -1,5 +1,14 @@
 /// <reference types="cypress" />
 
+// replaces any current HTML in the document with new html
+const writeHtml = (html) => {
+  cy.document().then((doc) => {
+    doc.open()
+    doc.write(html)
+    doc.close()
+  })
+}
+
 describe('TodoMVC API', () => {
   beforeEach(() => {
     cy.request('POST', '/reset')
@@ -26,13 +35,7 @@ describe('TodoMVC API', () => {
       )
 
     cy.log('**render HTML**')
-    cy.request('/')
-      .its('body')
-      .then((html) => {
-        cy.document().then((doc) => {
-          doc.write(html)
-        })
-      })
+    cy.request('/').its('body').then(writeHtml)
 
     // now that the server response is in the test runner
     // let's query it like a normal site
@@ -112,6 +115,77 @@ describe('TodoMVC API', () => {
         cy.request('POST', '/clear-completed')
         // confirm a single item remains
         cy.request('/todos').its('body').should('deep.equal', [todos[0]])
+      })
+  })
+
+  it('has todo page', () => {
+    cy.request('POST', '/', {
+      what: 'new todo',
+    })
+
+    cy.request('/todos')
+      .its('body')
+      .should('have.length', 1)
+      .its('0.id')
+      .then((id) => {
+        cy.log('**render todo page HTML**')
+        const url = `/todo/${id}`
+        cy.request(url).its('body').then(writeHtml)
+        // and confirm a single todo is shown and it is a link to itself
+        cy.get('.todo-list li')
+          .should('have.length', 1)
+          .first()
+          .find('label')
+          .should('have.text', 'new todo')
+          .find('a')
+          .should('have.attr', 'href', url)
+      })
+  })
+
+  it('has active page', () => {
+    cy.request('POST', '/', {
+      what: 'first todo',
+    })
+    cy.request('POST', '/', {
+      what: 'second todo',
+    })
+    cy.request('/todos')
+      .its('body')
+      .should('have.length', 2)
+      .then((todos) => {
+        cy.request('PATCH', '/', { id: todos[1].id, done: 'true' })
+
+        cy.log('**the first todo is active**')
+        cy.visit('/active')
+        cy.get('.todo-list li')
+          .should('have.length', 1)
+          .first()
+          .find('label')
+          .should('have.text', todos[0].what)
+      })
+  })
+
+  it('has completed page', () => {
+    cy.request('POST', '/', {
+      what: 'first todo',
+    })
+    cy.request('POST', '/', {
+      what: 'second todo',
+    })
+    cy.request('/todos')
+      .its('body')
+      .should('have.length', 2)
+      .then((todos) => {
+        cy.request('PATCH', '/', { id: todos[1].id, done: 'true' })
+
+        cy.log('**the second todo is completed**')
+        cy.visit('/completed')
+        cy.get('.todo-list li')
+          .should('have.length', 1)
+          .first()
+          .should('have.class', 'completed')
+          .find('label')
+          .should('have.text', todos[1].what)
       })
   })
 })
